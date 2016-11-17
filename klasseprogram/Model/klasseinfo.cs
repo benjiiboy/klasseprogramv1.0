6bcf44b7340c71787a59ac6f779455006ff4342b
benjamin lindgaard <benjaminlindgaard@ka-net.dk>
2016-11-17 09:54:26 +0100

    Tilføjede properties

---
+++ klasseprogram/Model/klasseinfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasseprogram.Model
+{
+    class klasseinfo
+    {
+        // Her laver jeg de properties som bruges til at beskrive den klasse vi skal lave. f.eks info om 1R.
+        public string ForNavn { get; set; }
+
+        public string EfterNavn { get; set; }
+
+        public int MobilNr { get; set; }
+
+        public string Email { get; set; }
+
+        public string GitNavn { get; set; }
+    }
+}
